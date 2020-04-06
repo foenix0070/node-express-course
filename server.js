21a4b979b0427ad7f1d554ff300d6e76cb4809b5
Cheick Abdoul-Razack NACRO <nacro.cheick@outlook.fr>
2020-04-06 13:07:23 +0000

    ajout de notre premiere requete de type get

--- server.js
+++ server.js
@@ -1,6 +1,21 @@
 const express = require('express');
 const app = express();
 
+const mockUserData=[
+  {name:'Jean'},
+  {name:'Konan'}
+  ];
+
+  app.get('/users', function(req,res){
+     res.json({
+        success: true,
+        message: 'Requête reussit!',
+        users: mockUserData
+     })
+  })
+
+
 app.listen(8000,function(){
     console.log("notre serveur tourne");
 });
+
