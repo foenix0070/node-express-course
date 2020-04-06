94cc7d9783a6f1b861719112b985d3094606acff
Cheick Abdoul-Razack NACRO <nacro.cheick@outlook.fr>
2020-04-06 13:29:07 +0000

    ajout de la route post avec body parser pour recuperer le body de request

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
+const bodyParser = require('body-parser'); /*cette bibliotheque est necessaire pour traiter les donnees venant du corps d'une requête*/
 const app = express();
+app.use(bodyParser.json());
 
 const mockUserData=[
   {name:'Jean'},
@@ -23,6 +25,26 @@ const mockUserData=[
     });
   });
 
+  app.post('/login', function(req, res){
+    const username=req.body.username;
+    const password=req.body.password;
+  
+    const mockUsername="billyTheKid";
+    const mockPassword="superSecret";
+  
+    if (username===mockUsername && password===mockPassword){
+         res.json({
+            success: true,
+            message: 'utilisateur connecté!',
+            token: 'brearer tokey5dhjg5gf8hfkhdfx1sdgl'
+         });
+    } else {
+         res.json({
+            success: false,
+            message: 'Mot de passe ou login incorrect...'
+         });
+    }
+  });
 
 app.listen(8000,function(){
     console.log("notre serveur tourne");
